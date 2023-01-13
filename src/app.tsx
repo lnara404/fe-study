c28bfbd13fb8a8e0c26a1637272d52781e78be09
lnara404 <naas0974@gmail.com>
2023-01-13 15:11:42 +0900

    :construction: 라우터 처리

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,9 @@
+import { useRoutes } from "react-router-dom";
+import { routes } from './routes';
+
 const App = () => {
-  return <div>앱</div>
+  const element = useRoutes(routes);
+  return element;
 }
 
 export default App;
